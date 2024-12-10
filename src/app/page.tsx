47385af1978c4b5752a03433c98166b2bdf1720a
femakin <santosfemakin@yahoo.com>
2024-12-10 20:11:55 +0100

    about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,7 @@
 // import Image from "next/image";
 
 import Image from "next/image";
-import headshot from '@/app/assets/headshot.jpeg'
-
-// export default function Home() {
-//   return (
-//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-//       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-//         <Image
-//           className="dark:invert"
-//           src="/next.svg"
-//           alt="Next.js logo"
-//           width={180}
-//           height={38}
-//           priority
-//         />
-//         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-//           <li className="mb-2">
-//             Get started by editing{" "}
-//             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-//               src/app/page.tsx
-//             </code>
-//             .
-//           </li>
-//           <li>Save and see your changes instantly.</li>
-//         </ol>
-
-//         <div className="flex gap-4 items-center flex-col sm:flex-row">
-//           <a
-//             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <Image
-//               className="dark:invert"
-//               src="/vercel.svg"
-//               alt="Vercel logomark"
-//               width={20}
-//               height={20}
-//             />
-//             Deploy now
-//           </a>
-//           <a
-//             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-//             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Read our docs
-//           </a>
-//         </div>
-//       </main>
-//       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-//         <a
-//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <Image
-//             aria-hidden
-//             src="/file.svg"
-//             alt="File icon"
-//             width={16}
-//             height={16}
-//           />
-//           Learn
-//         </a>
-//         <a
-//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <Image
-//             aria-hidden
-//             src="/window.svg"
-//             alt="Window icon"
-//             width={16}
-//             height={16}
-//           />
-//           Examples
-//         </a>
-//         <a
-//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-//           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <Image
-//             aria-hidden
-//             src="/globe.svg"
-//             alt="Globe icon"
-//             width={16}
-//             height={16}
-//           />
-//           Go to nextjs.org →
-//         </a>
-//       </footer>
-//     </div>
-//   );
-
-
-
+import headshot from "@/app/assets/head_shot_native.jpg";
 
 export default function Home() {
   return (
@@ -114,30 +12,85 @@ export default function Home() {
             <div className="gap-2 flex justify-between">
               <div className="flex-col flex flex-1 space-y-1.5">
                 <div className="flex">
-                  <span className="inline-block text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
-                  style={{opacity: 1, filter: 'blur(0px)', transform: 'translateY(-8px) translateZ(0px)'}}>Hi, I&apos;m Femi 👋</span>
+                  <span
+                    className="inline-block text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                    style={{
+                      opacity: 1,
+                      filter: "blur(0px)",
+                      transform: "translateY(-8px) translateZ(0px)",
+                    }}
+                  >
+                    Hi, I&apos;m Femi 👋
+                  </span>
                 </div>
                 <div className="flex">
-                  <span className="inline-block max-w-[600px] md:text-xl"  style={{opacity: 1, filter: 'blur(0px)', transform: 'translateY(-8px) translateZ(0px)'}}>Software Engineer | Javascript | Typescript.</span>
-                </div>
-
-              </div>
-
-              <div  style={{opacity: 1, filter: 'blur(0px)', transform: 'translateY(-8px) translateZ(0px)'}}>
-                <span className="relative flex shrink-0 overflow-hidden rounded-full size-28 border">
-                  <Image className="aspect-square h-full w-full" alt="Tosin Ogunfowote" src={headshot}>
-                    </Image>
+                  <span
+                    className="inline-block max-w-[600px] md:text-xl"
+                    style={{
+                      opacity: 1,
+                      filter: "blur(0px)",
+                      transform: "translateY(-8px) translateZ(0px)",
+                    }}
+                  >
+                    Frontend Engineer | Javascript | Typescript.
                   </span>
-
+                </div>
               </div>
 
+              <div
+                style={{
+                  opacity: 1,
+                  filter: "blur(0px)",
+                  transform: "translateY(-8px) translateZ(0px)",
+                }}
+              >
+                <span className="relative flex shrink-0 overflow-hidden rounded-full size-28 border">
+                  <Image
+                    className="aspect-square h-full w-full"
+                    alt="Tosin Ogunfowote"
+                    src={headshot}
+                  ></Image>
+                </span>
+              </div>
             </div>
           </div>
-
         </section>
 
+        <section id="about">
+          <div
+            style={{
+              opacity: 1,
+              filter: "blur(0px)",
+              transform: "translateY(-8px) translateZ(0px)",
+            }}
+          >
+            <h2 className="text-xl font-bold">About</h2>
+          </div>
+
+          <div
+            style={{
+              opacity: 1,
+              filter: "blur(0px)",
+              transform: "translateY(-8px) translateZ(0px)",
+            }}
+          >
+            <div className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+              <p className="text-[#737373]">
+               I am a frontend engineer with over 3 years of experience, holding a degree in mathematics and a Master of Science (MSc) in mathematics, and a strong
+                background in software development using JavaScript, TypeScript,
+                React, and NextJs amongst other tools. Over the years, I
+                have specialized in building and optimizing web applications in
+                several industries and startups, especially in the cloud
+                space, working with cross-functional teams to design solutions
+                that meet business goals and drive measurable results. My
+                commitment to best practices and attention to detail ensures
+                that my code not only meets standards but also pushes the
+                envelope in terms of performance and user engagement
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
 }
-
